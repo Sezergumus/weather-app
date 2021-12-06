20164cc0e58a59c8a7be227dd712a2581beea8ff
Sezergumus <66944231+Sezergumus@users.noreply.github.com>
2021-12-07 00:34:14 +0300

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1Ijoic2V6ZXJndW11cyIsImEiOiJja202MTZuOTgwN25vMnBtenNqb2R4aHhzIn0._BczkelpOS1noqF0G_U4gw&limit=1`
+    const url = `http://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1Ijoic2V6ZXJndW11cyIsImEiOiJja202MTZuOTgwN25vMnBtenNqb2R4aHhzIn0._BczkelpOS1noqF0G_U4gw&limit=1`
 
     request({ url, json: true }, (error, { body, } = {} ) => {
         if(error){
